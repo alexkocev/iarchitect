3be79e638125443b1855a13f1189b4ead9f1a835
maxime conti <maxime.conti@gmail.com>
2022-03-08 23:08:17 +0100

    correction erreur calcul taux (stratégie 5)

--- iarchitect/solutions/__init__.py
+++ iarchitect/solutions/__init__.py
@@ -1,27 +1,14 @@
-import json
-from argparse import Namespace
-from pathlib import Path
+from .common import ValidEnv,read_args,write_args
 from .aligned_rows_big import make_environment as aligned_rows_big_make_environment
 from .windows_env_big_network import make_environment as windows_env_big_network_make_environment
 
 
-def read_args(folder):
-    p = Path(folder) / "args.json"
-    d = dict()
-    print(p)
-    if p.exists():
-        with open(p,"r") as f:
-            d = json.load(f)
-    return Namespace(**d)
-
-VALID_NAME_ENV = ["aligned_rows_big","windows_env_big_network"]
 
 def make_environment(args):
     if "name_env" not in args:
-        setattr(args,"name_env",VALID_NAME_ENV[0])
+        setattr(args,"name_env",ValidEnv.aligned_rows_big)
     name = args.name_env
-    assert name in VALID_NAME_ENV
-    if name == VALID_NAME_ENV[0]:
+    if name == ValidEnv.aligned_rows_big:
         return aligned_rows_big_make_environment(args)
-    elif name == VALID_NAME_ENV[1]:
+    elif name == ValidEnv.windows_env_big_network:
         return windows_env_big_network_make_environment(args)
